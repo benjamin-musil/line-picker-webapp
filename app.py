090ed30e179687a338e4d01306c9196f9b89b079
ROFRANKLIN <robin.franklin@viasat.com>
2019-08-26 10:41:37 -0500

    init

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from pymongo import MongoClient
+from datetime import datetime
 
 
 app = Flask(__name__)
+
+
+def mongodb_get():
+    uri = "mongodb+srv://<username>:<password>@cluster0-wwuwc.mongodb.net/test?retryWrites=true&w=majority"
+    client = MongoClient(uri)
+    db = client.test
 
 
 @app.route('/example/endpoint', methods=['POST'])
