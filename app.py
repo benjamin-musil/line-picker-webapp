502a06f79aace84480f5d04f51ae5782c7994d39
Nathan Wiatrek <nwiatrek@gmail.com>
2019-08-26 22:44:18 -0500

    can connect to the db, need dns python

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
-
+from pymongo import MongoClient
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -8,6 +9,21 @@ app = Flask(__name__)
 def example_function():
     pass
 
+
+@app.route('/', methods=['GET'])
+def example_get():
+    # for the connection to work you need dnspython
+    db = mongodb_get()
+    print(db['Test Restaurants'])
+    return{'message': 'hello world'}
+
+
+
+def mongodb_get():
+    uri = 'mongodb+srv://apt-6-admin:h00k3m@cluster0-wwuwc.mongodb.net/test?retryWrites=true&w=majority'
+    client = MongoClient(uri)
+    db = client['Restaurants']
+    return db
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', port='5000')
