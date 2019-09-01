76324d79442883e5ee759dcf4c7bf3e89e77f409
ROFRANKLIN <robin.franklin@viasat.com>
2019-09-01 13:43:08 -0500

    did my endpoint

--- app.py
+++ app.py
@@ -1,32 +1,49 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 from pymongo import MongoClient
-import pprint
-import logging
-from datetime import datetime
-
 
 app = Flask(__name__)
 
 
-def mongodb_get():
-    uri = "mongodb+srv://rofranklin:Froggy01@cluster0-wwuwc.mongodb.net/test?retryWrites=true&w=majority"
+def mongodb_get_restaurants():
+    uri = 'mongodb+srv://apt-6-admin:h00k3m@cluster0-wwuwc.mongodb.net/test?retryWrites=true&w=majority'
     client = MongoClient(uri)
-    db = client.get_database('Restaurants')
-    collection_name = "Test Restaurants"
+    db = client['Restaurants']
+    collection_name = "Test Restaurants "
     collection = db[collection_name]
-    items = collection.find({"Name": "Taco Bell"})
+    return collection
+
+
+def mongo_get_wait_time_by_objectid(id):
+    uri = 'mongodb+srv://apt-6-admin:h00k3m@cluster0-wwuwc.mongodb.net/test?retryWrites=true&w=majority'
+    client = MongoClient(uri)
+    db = client['WaitTimes']
+    collection_name = "Test Wait Times"
+    collection = db[collection_name]
+    items = collection.find({"RestaurantId": str(id)})
+    json_arr = []
     for item in items:
-        logging.error(pprint.pprint(item))
+        json_arr.append(item)
+    print(json_arr)
+    wait_time = json_arr[0]['WaitTime'][0][0]
+    print(wait_time)
+    timestamp = json_arr[0]['WaitTime'][0][1]
+    return wait_time, timestamp
 
-    return(pprint.pprint(items))
 
-
-
-
-@app.route('/example/endpoint', methods=['GET'])
-def example_function():
-    db_instance = mongodb_get()
-    return jsonify(db_instance), 200
+@app.route('/getwait/byrestaurant', methods=['GET'])
+def get_wait():
+    content = request.args
+    name = content.get('Restaurant Name')
+    address = content.get('Address')
+    collection = mongodb_get_restaurants()
+    items = collection.find({"Address": address})
+    json_arr = []
+    for item in items:
+        json_arr.append(item)
+    object_id = json_arr[0]['_id']
+    print(object_id)
+    wait_time, timestamp = mongo_get_wait_time_by_objectid(object_id)
+    return jsonify(str(name) + ' has a wait time of ' + str(wait_time) + ' reported at ' + str(timestamp))
 
 
 if __name__ == '__main__':
