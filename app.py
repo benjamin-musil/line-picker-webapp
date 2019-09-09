d4dc7b9a59c783de1d20fa92799820550cf19001
Nathan Wiatrek <nwiatrek@gmail.com>
2019-09-08 21:34:18 -0500

    updating wording, adding tests, adding new more useful function

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, jsonify
-from pymongo import MongoClient
 from Models import Restaurant, MongoDb
 
 app = Flask(__name__)
@@ -7,12 +6,9 @@ app = Flask(__name__)
 # Can't do POST method until we have some kind of form that inputs data, since we're just testing db
 # interaction, we'll use the default GET for now
 # https://stackoverflow.com/questions/3477333/what-is-the-difference-between-post-and-get/3477374#3477374
-@app.route('/post/test',methods=['GET'])
+@app.route('/post/test', methods=['GET'])
 def example_post():
-    # for the connection to work you need dnspython
-    db = MongoDb.mongo_client('Restaurants')
-    collection = db['Users ']
-    
+    collection = MongoDb.mongo_collection('Users ')
     test_user = {
         'username'      : 'Ben',
         'email'         : 'benjamin.musil@gmail.com',
@@ -20,42 +16,27 @@ def example_post():
         'role'          : 'admin',
         'favorite_food' : 'breakfast'
     }
-    
-    post = collection.insert_one(test_restaurant)
-
+    collection.insert_one(test_user)
     return{'message': 'Post successful'}
+
 
 @app.route('/category/<category>', methods=['GET'])
 def example_get(category):
-    # for the connection to work you need dnspython
-    db = MongoDb.mongo_client('Restaurants')
-    # there is a space after the name, if you look on the website it doesn't look like there is though
-    # so that's really annoying
-    collection = db['Test Restaurants ']
+    collection = MongoDb.mongo_collection('Test Restaurants ')
     results = collection.find({u'Category': u''+category+''})
     restaurant_arr = []
     for document in results:
+        print(document)
         restaurant = Restaurant.from_document(document)
         restaurant_arr.append(restaurant.__dict__)
     return jsonify(restaurant_arr)
 
 
-def mongodb_get_restaurants():
-    uri = 'mongodb+srv://apt-6-admin:h00k3m@cluster0-wwuwc.mongodb.net/test?retryWrites=true&w=majority'
-    client = MongoClient(uri)
-    db = client['Restaurants']
-    collection_name = "Test Restaurants "
-    collection = db[collection_name]
-    return collection
-
-
-def mongo_get_wait_time_by_objectid(id):
-    uri = 'mongodb+srv://apt-6-admin:h00k3m@cluster0-wwuwc.mongodb.net/test?retryWrites=true&w=majority'
-    client = MongoClient(uri)
-    db = client['WaitTimes']
+def mongo_get_wait_time_by_objectid(restaurant_id):
+    db = MongoDb.mongo_database('WaitTimes')
     collection_name = "Test Wait Times"
     collection = db[collection_name]
-    items = collection.find({"RestaurantId": str(id)})
+    items = collection.find({"RestaurantId": str(restaurant_id)})
     json_arr = []
     for item in items:
         json_arr.append(item)
@@ -71,7 +52,7 @@ def get_wait():
     content = request.args
     name = content.get('Restaurant Name')
     address = content.get('Address')
-    collection = mongodb_get_restaurants()
+    collection = MongoDb.mongo_collection('Test Restaurants ')
     items = collection.find({"Address": address})
     json_arr = []
     for item in items:
@@ -80,7 +61,6 @@ def get_wait():
     print(object_id)
     wait_time, timestamp = mongo_get_wait_time_by_objectid(object_id)
     return jsonify(str(name) + ' has a wait time of ' + str(wait_time) + ' reported at ' + str(timestamp)), 200
-
 
 
 if __name__ == '__main__':
