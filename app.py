fc825a724313bcbb8b84a606f8e077f5a64485e2
Nathan Wiatrek <nwiatrek@gmail.com>
2019-09-11 19:56:05 -0500

    removing bad code

--- app.py
+++ app.py
@@ -93,29 +93,5 @@ def get_wait():
     return jsonify(str(name) + ' has a wait time of ' + str(wait_time) + ' reported at ' + str(timestamp)), 200
 
 
-@app.route('/', methods=['GET'])
-def example_get():
-    # for the connection to work you need dnspython
-    db = mongodb_get()
-    # there is a space after the name, if you look on the website it doesn't look like there is though
-    # so that's really annoying
-    collection = db['Test Restaurants ']
-    tacos = collection.find({u'Category': u'Tacos'})
-    json_arr = []
-    for i in tacos:
-        print(i)
-        json_arr.append(dumps(i))
-    print(json_arr)
-    return(jsonify(json_arr))
-    # return{'message': 'hello world'}
-
-
-
-def mongodb_get():
-    uri = 'mongodb+srv://apt-6-admin:h00k3m@cluster0-wwuwc.mongodb.net/test?retryWrites=true&w=majority'
-    client = MongoClient(uri)
-    db = client['Restaurants']
-    return db
-
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', port='5000')
