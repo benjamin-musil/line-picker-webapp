c46c1f11e5b4bf3645820e346176c539b8605d6c
Nathan Wiatrek <nwiatrek@gmail.com>
2019-10-03 22:31:08 -0500

    functionality done ish

--- Models/MongoDb.py
+++ Models/MongoDb.py
@@ -9,6 +9,7 @@ def mongo_database(database):
     """
     uri = 'mongodb+srv://apt-6-admin:h00k3m@cluster0-wwuwc.mongodb.net/test?retryWrites=true&w=majority'
     client = MongoClient(uri)
+    client.start_session()
     db = client[database]
     return db
 
--- Routes/restaurant_route.py
+++ Routes/restaurant_route.py
@@ -40,7 +40,7 @@ def add_restaurant():
 def submit_restaurant():
     form_args = request.form
     print(form_args)
-    restaurant = Restaurant.Restaurant('', form_args['Name'],  form_args['Address'], 'Taco', 'uk')
+    restaurant = Restaurant.Restaurant('', form_args['Name'],  form_args['Address'], form_args['category'], '-')
     restaurant.add_to_db()
     return redirect('/add-restaurant/')
 
